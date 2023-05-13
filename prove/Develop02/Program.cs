b65ae7f48e7c8cff8be123e6335ba2518c9c8133
Use your GitHub username <103981244+JayeRNeal@users.noreply.github.com>
2023-05-12 19:08:07 -0600

    5/12

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.I0;
 
 class Program
 {
@@ -82,44 +82,172 @@ class Program
         }
     }
 }
-
-public class Journal
+            public class Prompt
 {
-    private List<string> entries = new List<string>();
+    List<int> usedPrompts = new List<int>();
 
-    public void WriteEntry()
+    public string GetPrompts()
     {
-        Console.WriteLine("Enter your journal entry:");
-        string entry = Console.ReadLine();
-        entries.Add(entry);
-        Console.WriteLine("Entry added successfully!");
-    }
+      string prompt ="";
+      List<string> prompts = new List<string>() 
+      {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What goals do you want to accomplish today?",
+        "If you could stay anywhere in the world where would you stay and why?",
+        "What are you most excited about for today?",
+        "List 10 things you love about yourself.",
+        "Who has been your biggest insipation?",
+        "What is your dream job?",
+        "Who makes you happiest?",
+        "Who makes you laugh the most?",
+        "What are you most Thankful for?",
+        "What do you do when you are faced with challenges?",
+        "What are you most proud of?",
+        "What are some of your favorite memories as a child.",
+        "In what ways will you improve in your life?",
+        "What are somethings you would like to own?.",
+        "Write down a list of books to read, movies to watch, or skills you'd like to aquire.",
+        "Who would you like to meet?",
+        "What do you miss most about the past?",
+        "What are your strengths and weaknesses?",
+        "What are the most valuable life lessons you have learned?",
+        "How would you describe yourself to someone you’ve never met?",
+        "What advice would you give your younger self?",
+        "What is your favoirte quote and how have you applied it to your life?",
+      };
 
-    public void DisplayEntries()
-    {
-        if (entries.Count == 0)
+      Random rand = new Random();
+      int num = rand.Next(0, prompts.Count);
+
+      if (usedPrompts.Count() < 26)
+
+      {
+        if (!usedPrompts.Contains(num))
+
         {
-            Console.WriteLine("No entries found.");
+          usedPrompts.Add(num);
+          prompt = prompts[num];
         }
-        else
-        {
-            Console.WriteLine("Journal Entries:");
-            for (int i = 0; i < entries.Count; i++)
+
+        else{
+        GetPrompts();
+        }
+      }else
+      
+      {
+        Console.WriteLine("All done for today!");
+      }
+
+      return prompt;
+    }
+}
+        public class Journal
             {
-                Console.WriteLine($"Entry {i + 1}: {entries[i]}");
-            }
+            public List<Entry> entryList = new List<Entry>(); //List of New entries
+
+            public List<Entry> loadList = new List<Entry>(); //List of Loaded entries
+
+            private string fileName;
+
+        public Journal()
+        {
+  
         }
+        public void Write()
+        {
+            Entry entry = new Entry();
+            entry.Write();
+            entryList.Add(entry);
+        }  
+        public void Display()
+        {
+            foreach (Entry entry in entryList)  // Add all new entries to the Load List before displaying.
+        {
+        loadList.Add(entry);
+        }
+            foreach (Entry entry in loadList)  // Now that Load List has all entries (Old and New). Display all.
+        {
+            Console.WriteLine($"------------------------------");
+            Console.WriteLine($"Date: {entry._date} - Prompt: {entry._prompt}");
+            Console.WriteLine($"Response: {entry._entry}");
+            Console.WriteLine($"------------------------------");
+            Console.WriteLine();
+        }
+            }
+        public void Load()
+            {
+            Console.Write("Enter the Filename: ");
+            fileName = Console.ReadLine();
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            for (int x = 0; x < lines.Count(); x+=3){
+            Entry entry = new Entry();
+            entry._date = lines[x];
+            entry._prompt = lines[x+1];
+            entry._entry = lines[x+2];
+            loadList.Add(entry);
+}
+public void SaveNew()
+{
+        Console.Write("Enter the Filename: ");
+        fileName = Console.ReadLine();
+            foreach (Entry entry in entryList){
+            using (StreamWriter file = new StreamWriter(fileName,append:true))
+    {
+            file.WriteLine(entry._date);
+            file.WriteLine(entry._prompt);
+            file.WriteLine(entry._entry);
+    }
+  }
+}
+    public void SaveAll()
+  {
+    Console.Write("Enter the Filename: ");
+    fileName = Console.ReadLine();
+    foreach (Entry entry in entryList)  // Add all new entries to the Load List before displaying.
+      {
+        loadList.Add(entry);
+      }
+    foreach (Entry entry in loadList){
+      using (StreamWriter file = new StreamWriter(fileName,append:true))
+      {
+        file.WriteLine(entry._date);
+        file.WriteLine(entry._prompt);
+        file.WriteLine(entry._entry);
+      }
+    }
+  }
+}
+public class Entry
+{
+    public string _entry;
+
+    public string _date;
+
+    private DateTime _dateTime = DateTime.Now;
+
+    public string _prompt;
+
+    private Prompt _newPrompt = new Prompt();
+
+    public Entry()
+    {
+
     }
 
-    public void LoadEntries()
+    public void Write()
     {
-        // Implementation for loading entries from a file
-        Console.WriteLine("Loading entries...");
-    }
+      _date = _dateTime.ToShortDateString();
+      _prompt = _newPrompt.GetPrompts();
+      Console.WriteLine(_prompt);
+      Console.Write("Entry: ");
+      _entry = Console.ReadLine();
 
-    public void SaveEntries()
-    {
-        // Implementation for saving entries to a file
-        Console.WriteLine("Saving entries...");
-    }
+    }   
+}
+
+}
 }
