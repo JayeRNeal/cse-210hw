e94d648605655de09ce7195817c9fb77304bf55a
Use your GitHub username <103981244+JayeRNeal@users.noreply.github.com>
2023-06-23 23:52:06 -0600

    6/23

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,95 +2,95 @@ using System;
 
 class Program
 {
-       private static List<Goal> goals = new List<Goal>();
+    private static List<Goal> goals = new List<Goal>();
 
-       static int totalPoints = 0;
+    static int totalPoints = 0;
 
-       static void Main (string[] args)
-       {
-           int choice = 0;
-           while(choice !=6)
-           {
-               Console.WriteLine("n/Menu Options: ");
-               Console.WriteLine("1. Create New Goal");
-               Console.WriteLine("2. List Goals:");
-               Console.WriteLine("3. Save Goals:");
-               Console.WriteLine("4. Load Goals");
-               Console.WriteLine("5. Record Event");
-               Console.WriteLine("6. Quit");
+    static void Main (string[] args)
+    {
+        int choice = 0;
+        while(choice !=6)
+        {
+            Console.WriteLine("n/Menu Options: ");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals:");
+            Console.WriteLine("3. Save Goals:");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
 
-               Console.Write("Please select a choice");
-               string enter = Console.ReadLine();
-               choice = int.Parse(enter);
+            Console.Write("Please select a choice");
+            string enter = Console.ReadLine();
+            choice = int.Parse(enter);
 
-               if (choice == 1)
-               {
-                   string name;
-                   string description;
-                   int point;
-                   void StandardInput() //Problem, fix
-                   (
-                       Console.Write("What will you name your goal? "); //Problem, fix
-                       name = Console.Readline();
-                       Console.Writeline("How would you describe it? ");
-                       description = Console.Readline();
-                       Console.Write("The amount of points associated with your goal? ");
-                       point = int.Parse(Console.Readline()); //Problem,fix
-                   )
-
-                   int secondChoice = 0;
-                   Console.WriteLine("Your goals are: ");
-                   Console.WriteLine("1. Small Gaol: ");
-                   Console.Writeline("2. Religion Goal: ");
-                   Console.WriteLine("3. Dailiy Goal: ");
-
-                   Console.Write("What goal would you like to make? ");
-                   string secondChoice = Console.Readline(); //incorrect??
-                   secondChoice = int.Parse(secondEnter);  //mispelled or incorrect
-
-                   if (secondChoice == 1)
-                   {
-                       StandardInput();
-                       SmallGoal mySmall = SmallGoal(name, description, point);
-                       goals.Add(mySmall);
-                   }
-                   else if (secondChoice == 2)
-                   {
-                       StandardInput();
-                       ReligionGoal myReligion = new ReligionGoalGoal(name, description, point);
-                       goals.Add(myReligion);
-                   }
-                   else if (secondChoice == 3)
-                   {
-                       int target;
-                       int bouns;
-                       StandardInput();
-                       Console.Write("To get a bouns, how many times must you complete this goal?: ");
-                       target = int.Parse(Console.Readline());
-                       Console.WriteLine("What is the total amount of the bonus?: ");
-                       bonus = int.Parse(Console.Readline());
-                       CheckListGoal = myCheckList = new CheckListGoal(name, description, point, target, bouns);
-                       goals.Add(myCheckList);
-                   }
-                )
-                else if (choice == 2)
+            if (choice == 1)
+            {
+                string name;
+                string description;
+                int point;
+                void StandardInput()
                 {
-                    ListGoals();
+                    Console.Write("What will you name your goal? "); //Problem, fix
+                    name = Console.Readline();
+                    Console.Writeline("How would you describe it? ");
+                    description = Console.Readline();
+                    Console.Write("The amount of points associated with your goal? ");
+                    point = int.Parse(Console.Readline()); //Problem,fix
                 }
-                else if (choice == 3)
+
+                int secondChoice = 0;
+                Console.WriteLine("Your goals are: ");
+                Console.WriteLine("1. Small Gaol: ");
+                Console.Writeline("2. Religion Goal: ");
+                Console.WriteLine("3. Dailiy Goal: ");
+
+                Console.Write("What goal would you like to make? ");
+                string secondChoice = Console.Readline(); //incorrect??
+                secondChoice = int.Parse(secondEnter);  //mispelled or incorrect
+
+                if (secondChoice == 1)
                 {
-                    SaveGoal();
+                    StandardInput();
+                    SmallGoal mySmall = SmallGoal(name, description, point);
+                    goals.Add(mySmall);
                 }
-                else if (choice == 4)
+                else if (secondChoice == 2)
                 {
-                    LoadGoal();
+                    StandardInput();
+                    ReligionGoal myReligion = new ReligionGoalGoal(name, description, point);
+                    goals.Add(myReligion);
                 }
-                else if (choice == 5)
+                else if (secondChoice == 3)
                 {
-                    RecordEvent();
+                    int target;
+                    int bouns;
+                    StandardInput();
+                    Console.Write("To get a bouns, how many times must you complete this goal?: ");
+                    target = int.Parse(Console.Readline());
+                    Console.WriteLine("What is the total amount of the bonus?: ");
+                    bonus = int.Parse(Console.Readline());
+                    CheckListGoal = myCheckList = new CheckListGoal(name, description, point, target, bouns);
+                    goals.Add(myCheckList);
                 }
             }
+            else if (choice == 2)
+            {
+                ListGoals();
+            }
+            else if (choice == 3)
+            {
+                SaveGoal();
+            }
+            else if (choice == 4)
+            {
+                LoadGoal();
+            }
+            else if (choice == 5)
+            {
+                RecordEvent();
+            }
         }
+    }
 
     public static void ListGoals()
     {
@@ -156,11 +156,13 @@ class Program
         {
             goal.Status();
         }
-        Console.Write{"Which goal did you accomplish? "};
+        Console.Write("Which goal did you accomplish? ");
         int goalchoice = int.Parse(Console.ReadLine());
         totalPoints = goals[goalsChoice - 1].RecordEvent(totalPoints);
+        }
     }
-}
+
+ // exsessive }?
 
 //Goal
 public abstract class Goal
@@ -224,14 +226,14 @@ public class CheckListGoal : Goal
     private int _bonus;
 
 }
-    public CheckListGoal(string name, string description, int point, int target, int bonus) : base(name, description, point)
+    public CheckListGoal(string name, string description, int point, int target, int bonus) : base(name, description, point);
     {
         _target = target;
         _bonus = bonus;
         _count = 0;
     }
 
-    public CheckListGoal(string name, string description, int point, bool isComplete, int count, int target, int bonus) : base(name, description, point, isComplete)
+    public CheckListGoal(string name, string description, int point, bool isComplete, int count, int target, int bonus) : base(name, description, point, isComplete);
     {
         _target = target;
         _bonus = bonus;
@@ -269,7 +271,7 @@ public class CheckListGoal : Goal
     {
         return _target;
     } 
-}
+
 
 //Religon
 public class ReligionGoal : Goal
