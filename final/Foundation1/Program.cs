ebb70dfdf1957ff8dd973f02d0306bcf20912ee9
Use your GitHub username <103981244+JayeRNeal@users.noreply.github.com>
2023-07-07 12:04:24 -0600

    7/5

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,88 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
+class Program{
+    static void Main(string[] args){
+        //3 videos w/ comments
+        Video video1 = new Video("Video 1 title", "Video 1 Author", 60);
+        video1.Comments.Add(new Comment("First Commentor", "1st comment on video 1"));
+        video1.Comments.Add(new Comment("Second Commentor", "2nd comment on video 1"));
+        video1.Comments.Add(new Comment("Third Commentor", "3rd comment on video 1"));
+        videos.Add(video1);
+
+
+        Video video2 = new Video("Video 2 title", "Video 2 Author", 120);
+        video2.Comments.Add(new Comment("First Commentor", "1st comment on video 2"));
+        video2.Comments.Add(new Comment("Second Commentor", "2nd comment on video 2"));
+        video2.Comments.Add(new Comment("Third Commentor", "3rd comment on video 2"));
+        video2.Comments.Add(new Comment("Fourth Commentor", "4th comment on video 2"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Video 3 title", "Video 3 Author", 180);
+        video3.Comments.Add(new Comment("First Commentor", "1st comment on video 3"));
+        video3.Comments.Add(new Comment("Second Commentor", "2nd comment on video 3"));
+        video3.Comments.Add(new Comment("Third Commentor", "3rd comment on video 3"));
+        videos.Add(video3);
+
+        //iterate through videos & display info
+        foreach (Video video in videos){
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine{$"Author: {video._author}"}; //error? says ;expected but have a ; at end of each line.
+            Console.WriteLine($"Length: {video._lengthSeconds} seconds");
+            Console.WriteLine($"Number of comments: {video.NumComments()}");
+            Console.WriteLine();
+            Console.WriteLine("Comments: ");
+
+            foreach (Comment comment in video.Comments) {
+                Console.WriteLine($"{comment._name}: {comment._text}"); //or object value = Console.WriteLine($"{comment._name}: {comment._text}");
+            }
+            Console.WriteLine();
+        }
     }
+
+//Video class
+class Video{
+    public string _title;
+    public string _author;
+    public string _lengthSeconds;
+    public List<Comment>Comments;
+
+    public Video(string title, string author, int _lengthSeconds){
+        _title = title;
+        _author = author;
+        _lengthSeconds = _lengthSeconds;
+        Comments = new List<Comment>();
+    }
+
+    //returns
+    public int NumComments(){
+        return Comments.Count;
+    }
+    public string GetTitle(){
+        return _title;
+    }
+    public string GetAuthor(){
+        return _author;
+    }
+    public int GetLengthSeconds(){
+        return _lengthSeconds;
+    }
+}
+
+//Comment class
+class Comment{
+    public string _name;
+    public string _text;
+
+    public Comment(string name, string text){
+        _name = name;
+        _text = text;
+    }
+    public string GetName(){
+        return _name;
+    }
+    public string GetText(){
+        return _text;
+    }
+}
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,123 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+//Create orders
+        Order order1 = new Order(
+        new Customer("John Doe", new Address("1472 Ave", "Provo", "UT", "US")),
+        new[] {
+            new Product("Playstation 4", "PS4", 300.99f, 2),
+            new Product("Dell Computer", "DC", 700.99f, 3),
+        });
+        Order order2 = new Order(
+        new Customer("Jane Doe", new Address("1572 Service Rd", "Orem", "UT", "US")),
+        new[] {
+            new Product("Basket", "BSK", 30.99f, 1),
+            new Product("Monopoly", "MPY", 24.99f, 2),
+            new Product("Blinds", "BLD", 49.99f, 5),
+        });
+        Order order2 = new Order(
+        new Customer("Christopher Columbus", new Address("1492 Ocean Blue St", "Valladolid", "Madrid", "ES")),
+        new[] {
+            new Product("Shoes", "SH", 30.99f, 4),
+            new Product("Laces", "LC", 5.99f, 2),
+            new Product("Shoe Cleaner", "SHC", 17.99f, 13),
+        });    
+
+// Display info
+        Console.WriteLine("\nOrder 1");
+        Console.WriteLine("-----------");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        string v = $"Total Price: ${order1.GetTotalPrice()}";
+        Console.WriteLine(v);
+
+        Console.WriteLine("\nOrder 2");
+        Console.WriteLine("-----------");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        string z = $"Total Price: ${order2.GetTotalPrice()}";
+        Console.WriteLine(z);
+
+        Console.WriteLine("\nOrder 3");
+        Console.WriteLine("-----------");
+        Console.WriteLine(order3.GetPackingLabel());
+        Console.WriteLine(order3.GetShippingLabel());
+        string x = $"Total Price: ${order3.GetTotalPrice()}";
+        Console.WriteLine(x);
     }
 }
+
+ //Order class
+ class Order{
+
+    private Product[] _products;
+    private Customer _customer;
+
+    public Order(Customer customer, Product[] products){
+         _customer = customer;
+         _products = products;
+    }
+
+    public float GetTotalPrice(){
+        float subtotal = 0;
+        foreach (var product in _products)
+        {
+            subtotal += product.GetPrice() * product.GetQuantity();
+        }
+        float shippingCost = _customer.GetAddress().IsInUSA() ? 5f : 35f;
+        return subtotal + shippingCost;
+    }
+    public string GetPackingLabel()
+    {
+    var label = ($"Packing Label:\n");        
+    foreach (var product in _products)
+    {
+        label += ($"{product.GetName()} ({product.GetID()})\n");
+    }
+    return label;
+    }
+    public string GetShippingLabel()
+    {
+    return ($"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress().ToString()}");
+    }
+ }
+
+ //Products class
+ class Product
+{
+    private string _name;
+    private string _id;
+    private float _price;
+    private int _quantity;
+
+    public Product(string name, string id, float price, int quantity)
+    {
+        _name = name;
+        _id = id;
+        _price = price;
+        _quantity = quantity;
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+    public string GetID()
+    {
+        return _id;
+    }
+    public float GetPrice()
+    {
+        return _price;
+    }
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+}
+
+//Customer class
+
+
+
+
+//Address class
